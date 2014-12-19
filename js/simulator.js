33886238c07febec2f58dac4ba8723ff10ab6fc3
mike <mpgarate@gmail.com>
2014-12-19 11:10:37 -0500

    debugging infinite loop

--- js/simulator.js
+++ js/simulator.js
@@ -1,9 +1,24 @@
-var City = function(deathRate) {
-    this.deathRate = deathRate;
-    this.getDeathCountList();
+var City = function(deathRate, sliderId, dataset) {
+  this.deathRate = deathRate;
+  this.dataset = dataset;
+  this.getDeathCountList();
+  this.sliderSelector = "#" + sliderId;
+
+  var city = this;
+
+  $(this.sliderSelector).slider({
+    values: [ deathRate ],
+    change: function(event, ui){
+      console.log(ui);
+      // simulation.drawChart();
+    }
+  });
 };
 
 City.prototype.getDeathCountList = function(){
+
+  this.deathrate = $(this.sliderSelector).slider("option", "value");
+
   var population = 100000;
   var deaths = 1;
   var generations = 0;
@@ -12,63 +27,89 @@ City.prototype.getDeathCountList = function(){
   deaths_list.push(deaths);
 
   while(population > 0){
-    deaths = Math.ceil((1 + this.deathRate) * deaths);
+    deaths = Math.ceil(this.deathrate * deaths);
     population = population - deaths;
 
     generations++;
     deaths_list.push(deaths);
   }
 
-  console.log(deaths_list);
-
   this.duration = deaths_list.length;
   return deaths_list;
 }
 
-$( "#slider" ).slider({
-  values: [ 67 ]
-});
-
-var pistoria = new City(35 * 0.1 * 5);
-var milan = new City(35 * 0.1 * 1);
-
-var context = $("#sim_chart").get(0).getContext("2d");
-
-var labels = [];
-
-for (var i = 0; i < pistoria.duration || i < milan.duration; i++){
-  labels.push(i);
+City.prototype.getChartDataset = function(){
+  this.dataset.data = this.getDeathCountList();
+  return this.dataset;
 }
 
-console.log(labels);
+var Simulation = function(cities){
+  this.cities = cities;
+  this.updateChartWidth();
+}
 
-var data = {
-  labels: labels,
-  datasets: [
-    {
-      label: "Pistoia: prevention and coping",
-      fillColor: "rgba(220,220,220,0.2)",
-      strokeColor: "rgba(220,220,220,1)",
-      pointColor: "rgba(220,220,220,1)",
-      pointStrokeColor: "#fff",
-      pointHighlightFill: "#fff",
-      pointHighlightStroke: "rgba(220,220,220,1)",
-      data: pistoria.getDeathCountList()
-    },
-    {
-      label: "Milan: Quarantine",
-      fillColor: "rgba(151,187,205,0.2)",
-      strokeColor: "rgba(151,187,205,1)",
-      pointColor: "rgba(151,187,205,1)",
-      pointStrokeColor: "#fff",
-      pointHighlightFill: "#fff",
-      pointHighlightStroke: "rgba(151,187,205,1)",
-      data: milan.getDeathCountList()
+Simulation.prototype.updateChartWidth = function(){
+  var chartWidth = 0;
+
+  for (var i = 0; i < this.cities.length; i++){
+    var cityOutbreakLength = cities[i].duration;
+    if (cityOutbreakLength > chartWidth){
+      chartWidth = cityOutbreakLength;
     }
-  ]
-};
+  }
 
+  this.chartWidth = chartWidth;
+}
 
-var chart = new Chart(context).Line(data);
+Simulation.prototype.drawChart = function(){
+  var context = $("#sim_chart").get(0).getContext("2d");
+  var labels = [];
 
+  for (var i = 0; i < this.chartWidth; i++){
+    labels.push(i);
+  }
+
+  var datasets = [];
+
+  for (var i = 0; i < this.cities.length; i++){
+    datasets.push(cities[i].getChartDataset(data));
+  }
+
+  var data = {
+    labels: labels,
+    datasets: datasets
+  };
+
+  var chart = new Chart(context).Line(data);
+  return chart;
+}
+
+var pistoriaDataset = {
+  label: "Pistoia: prevention and coping",
+  fillColor: "rgba(220,220,220,0.2)",
+  strokeColor: "rgba(220,220,220,1)",
+  pointColor: "rgba(220,220,220,1)",
+  pointStrokeColor: "#fff",
+  pointHighlightFill: "#fff",
+  pointHighlightStroke: "rgba(220,220,220,1)"
+}
+
+var milanDataset = {
+  label: "Milan: Quarantine",
+  fillColor: "rgba(151,187,205,0.2)",
+  strokeColor: "rgba(151,187,205,1)",
+  pointColor: "rgba(151,187,205,1)",
+  pointStrokeColor: "#fff",
+  pointHighlightFill: "#fff",
+  pointHighlightStroke: "rgba(151,187,205,1)"
+}
+
+var pistoria = new City(35 * 0.1 * 5, "pistoria-slider", pistoriaDataset);
+var milan = new City(35 * 0.1 * 1, "milan-slider", milanDataset);
+
+var cities = [pistoria, milan];
+
+var simulation = new Simulation(cities);
+
+var chart = simulation.drawChart();
 
